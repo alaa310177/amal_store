5485e6c669ebb067f9001019fc27a1b626b6964c
Alaa <theqa1995theqa@gmail.com>
2026-08-24 09:06:32 +0300

    add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5PZG4C7K19"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-5PZG4C7K19');
+          `
+        }} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
